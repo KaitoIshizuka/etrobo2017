f480c829dc337cb1771394ad486b35578e7eefbd
KaitoIshizuka <e155716@ie.u-ryukyu.ac.jp>
2017-07-24 14:48:04 +0900

    Calibration 作成

---
+++ Calibration.h
@@ -0,0 +1,27 @@
+#include "ev3api.h"
+#include "ColorSensor.h"
+
+using ev3api::ColorSensor;
+
+class Calibration {
+
+  public:
+	Calibration();
+	float calcPid();
+	float calcMotorPid();
+	void setLineRL(int setRL);
+  private:
+	int LineRL;
+	ColorSensor colorSensor;
+	WheelCtrl wheelCtrl;
+	const float delta_t = 0.004;
+	const float Kp = 0.63;
+	const float Ki = 0.0;
+	const float Kd = 0.0;
+	int diff[2];
+	int motor_diff[2];
+	float integral;
+	float motor_integral;
+	unsigned int target_val;
+};
+
